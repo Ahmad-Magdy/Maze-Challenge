c4d544e6f14eafead11b54d8bf669454fed272ab
Ahmad Magdy <ahmedmagdi@outlook.com>
2018-07-19 21:25:18 +0200

    add home component and setup routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { StartPlayingComponent } from './start-playing/start-playing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: HomeComponent },
+  { path: 'get-started', component: StartPlayingComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
