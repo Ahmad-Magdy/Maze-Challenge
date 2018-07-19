46587748d354af7ed2bc2f6fd23ddadde170fbfe
Ahmad Magdy <ahmedmagdi@outlook.com>
2018-07-19 21:24:42 +0200

    create start-playing

---
+++ src/app/start-playing/start-playing.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StartPlayingComponent } from './start-playing.component';
+
+describe('StartPlayingComponent', () => {
+  let component: StartPlayingComponent;
+  let fixture: ComponentFixture<StartPlayingComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ StartPlayingComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StartPlayingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
